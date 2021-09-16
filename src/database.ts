a074697ec85183352c80f52999dadeeccff19692
henrique945 <henrique.rodrigues7@hotmail.com>
2021-09-16 19:43:59 -0300

    create(entity): create entity, get body and create example.db

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,5 @@
 import { Connection, ConnectionOptions, createConnection } from 'typeorm';
+import { TempEntity } from './entities/temp.entity';
 
 export async function getDatabaseConnection(): Promise<Connection> {
   const options: ConnectionOptions = {
@@ -6,7 +7,11 @@ export async function getDatabaseConnection(): Promise<Connection> {
     type: 'sqlite',
     database: process.env.DB_PATH || '',
     logger: 'advanced-console',
-  }
+    entities: [
+      TempEntity,
+    ],
+    synchronize: true,
+  };
 
   return await createConnection(options);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { getDatabaseConnection } from './database';
+import { TempEntity } from './entities/temp.entity';
 
 require('dotenv').config();
 const express = require('express');
 const app = express();
+
+app.use(express.json())
 
 const createApp = async () => {
   const db = await getDatabaseConnection();
@@ -14,6 +17,14 @@ const createApp = async () => {
   app.get('/temp', (req, res) => {
     console.log(req.params);
     res.json({ temperature: 10 });
+  });
+
+  app.post('/temp', async (req, res) => {
+    console.log(req.body);
+
+    const tempRepository = db.getRepository(TempEntity);
+    const result = await tempRepository.save({ avg: 50, name: 'Henrique' });
+    res.json(result);
   });
 
   app.listen(process.env.PORT, () => {
